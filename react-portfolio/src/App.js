a6eb6ae96e3cb446d49bf1d4e8691ed2bb71ba1e
hmustain <hunter@bunkerbranding.com>
2023-03-08 10:04:18 -0600

    saving navbar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,5 +1,15 @@
 // We import useState and useEffect in our component
 import React, { useState, useEffect } from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* The rest of your React application goes here */}
+    </div>
+  );
+}
 
 
 
---
+++ react-portfolio/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+      <header id="header" className="fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-black fixed-top py-4">
+          <div className="container justify-content-center">
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav mx-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#home">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#about">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#services">Services</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#projects">Projects</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  }
+  
+  export default Navbar;
