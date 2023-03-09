4bed48b35defd05144dc460167f98fa1efb625a7
hmustain <hunter@bunkerbranding.com>
2023-03-09 09:04:10 -0600

    updating photos for portfolio after askbcs section also adding link to github per readme

--- react-portfolio/src/components/Projects.js
+++ react-portfolio/src/components/Projects.js
@@ -1,5 +1,12 @@
 import React from "react";
 import '../styles/Projects.css';
+import work1 from '../assets/images/work-1.jpg';
+import work2 from '../assets/images/work-2.jpg';
+import work3 from '../assets/images/work-3.jpg';
+import work4 from '../assets/images/work-4.jpg';
+import work5 from '../assets/images/work-5.jpg';
+import work6 from '../assets/images/work-6.jpg';
+
 
 function Projects() {
     return (
@@ -21,9 +28,9 @@ function Projects() {
             <div className="row">
               <div className="col-md-4">
                 <div className="work-box">
-                  <a href="../assets/images/work-1.jpg" data-gallery="portfolioGallery" className="portfolio-lightbox">
+                  <a href={work1} data-gallery="portfolioGallery" className="portfolio-lightbox">
                     <div className="work-img">
-                      <img src="../assets/images/work-1.jpg" alt="" className="img-fluid"/>
+                      <img src={work1} alt="" className="img-fluid"/>
                     </div>
                   </a>
                   <div className="work-content">
@@ -31,7 +38,8 @@ function Projects() {
                       <div className="col-sm-8">
                         <h2 className="w-title">Horiseon Website</h2>
                         <div className="w-more">
-                          <span className="w-ctegory">Front End</span> / <span className="w-date">Oct 2022</span>
+                        <span className="w-ctegory"><a href="https://github.com/hmustain/02-Challenge">GitHub</a></span> / <span className="w-date">Oct 2022</span>
+
                         </div>
                       </div>
                       <div className="col-sm-4">
@@ -45,9 +53,9 @@ function Projects() {
               </div>
               <div className="col-md-4">
       <div className="work-box">
-        <a href="" data-gallery="portfolioGallery" className="portfolio-lightbox">
+        <a href={work4} data-gallery="portfolioGallery" className="portfolio-lightbox">
           <div className="work-img">
-            <img src="" alt="" className="img-fluid" />
+            <img src={work4} alt="" className="img-fluid" />
           </div>
         </a>
         <div className="work-content">
@@ -55,7 +63,7 @@ function Projects() {
             <div className="col-sm-8">
               <h2 className="w-title">Basic Portfolio</h2>
               <div className="w-more">
-                <span className="w-ctegory">Front End</span> / <span className="w-date">Oct 2022</span>
+              <span className="w-ctegory"><a href="https://github.com/hmustain/Hunter-Mustain-Portfolio">GitHub</a></span> / <span className="w-date">Oct 2022</span>
               </div>
             </div>
             <div className="col-sm-4">
@@ -69,9 +77,9 @@ function Projects() {
     </div>
     <div className="col-md-4">
   <div className="work-box">
-    <a href=" "data-gallery="portfolioGallery" className="portfolio-lightbox">
+    <a href={work3} data-gallery="portfolioGallery" className="portfolio-lightbox">
       <div className="work-img">
-        <img src="" alt="" className="img-fluid" />
+        <img src={work3} alt="" className="img-fluid" />
       </div>
     </a>
     <div className="work-content">
@@ -79,7 +87,7 @@ function Projects() {
         <div className="col-sm-8">
           <h2 className="w-title">Password Generator</h2>
           <div className="w-more">
-            <span className="w-ctegory">Front End</span> / <span className="w-date">Nov 2022</span>
+          <span className="w-ctegory"><a href="https://github.com/hmustain/Password-Generator">GitHub</a></span> / <span className="w-date">Nov 2022</span>
           </div>
         </div>
         <div className="col-sm-4">
@@ -93,9 +101,9 @@ function Projects() {
 </div>
 <div className="col-md-4">
   <div className="work-box">
-    <a href="" data-gallery="portfolioGallery" className="portfolio-lightbox">
+    <a href={work4} data-gallery="portfolioGallery" className="portfolio-lightbox">
       <div className="work-img">
-        <img src="" alt="" className="img-fluid" />
+        <img src={work4} alt="" className="img-fluid" />
       </div>
     </a>
     <div className="work-content">
@@ -103,7 +111,7 @@ function Projects() {
         <div className="col-sm-8">
           <h2 className="w-title">National Park Locator</h2>
           <div className="w-more">
-            <span className="w-ctegory">Front End</span> / <span className="w-date">Dec 2022</span>
+          <span className="w-ctegory"><a href="https://github.com/hmustain/National-Park-Locator">GitHub</a></span> / <span className="w-date">Dec 2022</span>
           </div>
         </div>
         <div className="col-sm-4">
@@ -117,9 +125,9 @@ function Projects() {
 </div>
 <div className="col-md-4">
   <div className="work-box">
-    <a href="" data-gallery="portfolioGallery" className="portfolio-lightbox">
+    <a href={work3} data-gallery="portfolioGallery" className="portfolio-lightbox">
       <div className="work-img">
-        <img src="" alt="" className="img-fluid" />
+        <img src={work3} alt="" className="img-fluid" />
       </div>
     </a>
     <div className="work-content">
@@ -127,7 +135,7 @@ function Projects() {
         <div className="col-sm-8">
           <h2 className="w-title">Note Taker</h2>
           <div className="w-more">
-            <span className="w-ctegory">Back End</span> / <span className="w-date">Jan 2023</span>
+          <span className="w-ctegory"><a href="https://github.com/hmustain/Note-Taker">GitHub</a></span> / <span className="w-date">Jan 2023</span>
           </div>
         </div>
         <div className="col-sm-4">
@@ -141,9 +149,9 @@ function Projects() {
 </div>
 <div className="col-md-4">
       <div className="work-box">
-        <a href="" data-gallery="portfolioGallery" className="portfolio-lightbox">
+        <a href={work6} data-gallery="portfolioGallery" className="portfolio-lightbox">
           <div className="work-img">
-            <img src="" alt="" className="img-fluid" />
+            <img src={work6} alt="" className="img-fluid" />
           </div>
         </a>
         <div className="work-content">
@@ -151,7 +159,7 @@ function Projects() {
             <div className="col-sm-8">
               <h2 className="w-title">The Cassette Tape</h2>
               <div className="w-more">
-                <span className="w-ctegory">Back End</span> / <span className="w-date">Feb 2023</span>
+              <span className="w-ctegory"><a href="https://github.com/hmustain/The-Cassette-Tape">GitHub</a></span> / <span className="w-date">Feb 2023</span>
               </div>
             </div>
             <div className="col-sm-4">
